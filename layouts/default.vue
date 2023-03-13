d642dbc1f97a0aef201d6645eaf7153328b978ab
olen-d <olen.daelhousen@outlook.com>
2023-03-12 20:14:33 -0400

    Add auth to clear cookie route

--- layouts/default.vue
+++ layouts/default.vue
@@ -37,7 +37,7 @@
   const logout = async () => {
     authStore.$reset()
     
-    const endpoint = 'api/v1/token/refresh/clear-cookie'
+    const endpoint = 'api/v1/auth/token/refresh/clear-cookie'
     const errorMessage = 'unable to clear refresh token cookie'
 
     const { error, isLoading, fetchResult, refresh } = await useFetchGet(null, config, errorMessage, endpoint)
