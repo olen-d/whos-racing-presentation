c0a907dca2fe763a6b7054af7ffb8dfa0b948ba3
olen-d <olen.daelhousen@outlook.com>
2023-03-29 23:35:18 -0400

    Add cookie to Form Login and Auth Role User

--- middleware/authRoleUser.js
+++ middleware/authRoleUser.js
@@ -1,5 +1,11 @@
 export default defineNuxtRouteMiddleware(async (to, from) => {
   const authStore = useAuthStore()
+  
+  if (process.server) {
+    const refreshTokenSPA = eventHandler((event) => getCookie(event, 'refreshTokenSPA'))
+    authStore.currentRefreshToken = refreshTokenSPA
+  }
+
   await authStore.checkBearerExpiration()
 
   const { isAuthorized, role } = authStore
